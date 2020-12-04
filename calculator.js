c9daca3713619f75cce67bbf47d3bffd8066671d
sj-foster <sj-foster@outlook.com>
2020-12-04 16:47:55 +0000

    Basic calculator html, css and simple functions

---
+++ calculator.js
@@ -0,0 +1,28 @@
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
+
+function operate(x, y, operator) {
+    return operator(x, y);
+}
+
+
+
+// console.log(add(1, 1));
+// console.log(subtract(1, 1));
+// console.log(multiply(2, 4));
+// console.log(divide(4, 2));
+
+// console.log(operate(1, 2, add))
