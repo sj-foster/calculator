83b8b40dfb2a3c14bada644ce5915bc2c6ff831f
sj-foster <sj-foster@outlook.com>
2020-12-05 09:14:16 +0000

    Added basic styling and js to display clicked buttons

--- calculator.js
+++ calculator.js
@@ -18,6 +18,59 @@ function operate(x, y, operator) {
     return operator(x, y);
 }
 
+let display = document.getElementById("calc-display");
+
+let calculatedValue = 0;
+
+function getValue(event) {
+    if (display.value === "0" && event.target.value != ".") {
+        display.value = event.target.value;
+    } else {
+        display.value += event.target.value;
+    }
+}
+
+function allClear() {
+    calculatedValue = 0;
+    display.value = 0;
+}
+
+function clear() {
+    display.value = 0;
+}
+
+const numButtons = document.getElementsByClassName("btn-num");
+
+for (var i = 0; i < numButtons.length; i++) {
+    numButtons[i].addEventListener('click', getValue)
+}
+
+const equalsButton = document.getElementsByClassName("equals")
+
+equalsButton[0].addEventListener('click', getValue)
+
+const allClearButton = document.getElementsByClassName("allclear")
+
+allClearButton[0].addEventListener('click', allClear)
+
+const clearButton = document.getElementsByClassName("clear")
+
+clearButton[0].addEventListener('click', clear);
+
+const operatorButtons = document.getElementsByClassName("operator");
+
+for (var i = 0; i < operatorButtons.length; i++) {
+    operatorButtons[i].addEventListener('click', getValue)
+}
+
+const decimalButton = document.getElementsByClassName("decimal");
+
+
+for (var i = 0; i < decimalButton.length; i++) {
+    decimalButton[i].addEventListener('click', getValue)
+}
+
+
 
 
 // console.log(add(1, 1));
