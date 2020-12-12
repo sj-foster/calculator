af318348c05fc0145e70089e7a8bb221f4e3bccd
sj-foster <sj-foster@outlook.com>
2020-12-12 11:29:20 +0000

    Caluclator functions mostly working

--- calculator.js
+++ calculator.js
@@ -1,5 +1,5 @@
 function add(x, y) {
-    return x + y;
+    return Number(x) + Number(y);
 }
 
 function subtract(x, y) {
@@ -11,27 +11,39 @@ function multiply(x, y) {
 }
 
 function divide(x, y) {
+    if (y == 0) {
+        return "ERROR";
+    }
     return x / y;
 }
 
 function operate(x, y, operator) {
-    return operator(x, y);
+    if (operator === "multiply") {
+        return multiply(x, y)
+    } 
+    if (operator === "add") {
+        return add(x, y);
+    }
+    if (operator === "subtract") {
+        return subtract(x, y);
+    }
+    if (operator === "divide") {
+        return divide(x, y);
+    }
 }
 
 let display = document.getElementById("calc-display");
 
-let calculatedValue = 0;
-
-function getValue(event) {
-    if (display.value === "0" && event.target.value != ".") {
-        display.value = event.target.value;
-    } else {
-        display.value += event.target.value;
-    }
-}
+let inputValue = null;
+let calculatedValue = null;
+let currentOperator = "";
+let displayCalculated = true;
 
 function allClear() {
-    calculatedValue = 0;
+    currentOperator = "";
+    inputValue = 0;
+    calculatedValue = null;
+    displayCalculated = true;
     display.value = 0;
 }
 
@@ -39,15 +51,64 @@ function clear() {
     display.value = 0;
 }
 
+function setDisplay(e) {
+    console.log(displayCalculated)
+    if (displayCalculated) {
+        display.value = e.target.value;
+        displayCalculated = false;
+    }
+    else {
+        display.value += e.target.value;
+        displayCalculated = false;
+    }
+}
+
+function displayFormat(out) {
+    console.log(typeof(out));
+    let outStr = out.toString();
+    console.log("outStr", outStr)
+    console.log("outStr.length", outStr.length)
+    if (outStr.length > 9) {
+        decimalLocation = outStr.indexOf(".")
+        console.log("decimalLocation", decimalLocation)
+        if (decimalLocation > -1) {
+            console.log("decimal", decimalLocation);
+            let decimalPlaces = 9 - decimalLocation;
+            let fixedOut = Number(outStr).toFixed(decimalPlaces);
+            console.log("fixedOut", fixedOut);
+            return fixedOut;
+        }
+    }
+    else {
+        return outStr
+    }
+}
+
+function setOperator(e) {
+    currentOperator = e.target.value;
+    // console.log(currentOperator)
+}
 const numButtons = document.getElementsByClassName("btn-num");
 
 for (var i = 0; i < numButtons.length; i++) {
-    numButtons[i].addEventListener('click', getValue)
+    numButtons[i].addEventListener('click', (e) => {
+        setDisplay(e);
+    });
 }
 
 const equalsButton = document.getElementsByClassName("equals")
 
-equalsButton[0].addEventListener('click', getValue)
+equalsButton[0].addEventListener('click', (e) => {
+    inputValue = display.value;
+    calculatedValue = operate(calculatedValue, inputValue, currentOperator);
+    // console.log(calculatedValue)
+    display.value = displayFormat(calculatedValue)
+    currentOperator = "";
+    inputValue = 0;
+    calculatedValue = null;
+    displayCalculated = true;
+    
+});
 
 const allClearButton = document.getElementsByClassName("allclear")
 
@@ -60,22 +121,28 @@ clearButton[0].addEventListener('click', clear);
 const operatorButtons = document.getElementsByClassName("operator");
 
 for (var i = 0; i < operatorButtons.length; i++) {
-    operatorButtons[i].addEventListener('click', getValue)
+    operatorButtons[i].addEventListener('click', (e) => {
+        inputValue = display.value;
+        
+        if (calculatedValue === null) {
+            calculatedValue = inputValue;
+            setOperator(e)
+        } else { 
+            calculatedValue = operate(calculatedValue, inputValue, currentOperator)
+            setOperator(e)
+        }
+        // console.log(calculatedValue)
+        displayCalculated = true;
+    });
 }
 
 const decimalButton = document.getElementsByClassName("decimal");
 
-
 for (var i = 0; i < decimalButton.length; i++) {
-    decimalButton[i].addEventListener('click', getValue)
+    decimalButton[i].addEventListener('click', (e) => {
+        if (display.value.indexOf(".") <= -1) {
+            // console.log("setting .")
+            setDisplay(e)
+        }
+    });
 }
-
-
-
-
-// console.log(add(1, 1));
-// console.log(subtract(1, 1));
-// console.log(multiply(2, 4));
-// console.log(divide(4, 2));
-
-// console.log(operate(1, 2, add))
